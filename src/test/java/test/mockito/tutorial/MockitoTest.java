f58c4080d7191d59c55dfe4a567ce532a2bb3e38
Venkateshwaran <venkateshwaran@Venkateshwarans-MacBook-Pro.local>
2020-03-09 12:48:58 +0100

    mockito tutorial

---
+++ src/test/java/test/mockito/tutorial/MockitoTest.java
@@ -0,0 +1,64 @@
+package test.mockito.tutorial;
+
+
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
+import test.mockito.tutorial.service.APIDataManagementInterface;
+
+
+
+
+public class MockitoTest {
+
+
+	APIDataManagementInterface dataAPI= Mockito.mock(APIDataManagementInterface.class);
+	
+
+	@Before
+	public void setup() {
+		when(dataAPI.Db_TEST_Insert("key", "value")).thenReturn(0);
+		when(dataAPI.Db_TEST_Insert("", "value")).thenReturn(1);
+		when(dataAPI.Db_TEST_Insert("key1", "value1")).thenReturn(0);
+		HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
+		hashmap.put(1, "key");
+		when(dataAPI.Db_TEST_Select()).thenReturn(hashmap);
+		
+	}
+	
+
+	@Test
+	public void testDb_TEST_Insert() throws IOException {
+		assertEquals(0, dataAPI.Db_TEST_Insert("key", "value"));
+		verify(dataAPI).Db_TEST_Insert("key", "value");
+	}
+	
+	
+	/**
+	 * Fails because the API returns 1 for empty key because of exception
+	 * @throws IOException
+	 */
+	@Test
+	public void testDb_TEST_Insert_Empty_key() throws IOException {
+		assertEquals(0, dataAPI.Db_TEST_Insert("key", "value"));
+		verify(dataAPI).Db_TEST_Insert("key", "value");
+	}
+	
+	@Test
+	public void testDb_TEST_Select() throws IOException {
+		HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
+		hashmap.put(1, "key");
+		assertEquals(hashmap, dataAPI.Db_TEST_Select());
+		verify(dataAPI).Db_TEST_Select();
+	}
+}
---
+++ src/test/java/test/mockito/tutorial/service/APIDataManagementInterface.java
@@ -0,0 +1,22 @@
+package test.mockito.tutorial.service;
+
+import java.util.HashMap;
+
+public interface APIDataManagementInterface {
+
+	
+	/**
+	 * @param name
+	 * @param value
+	 * @return 0 if its properly inserted in db
+	 * 		   1 if any exception occurs
+	 */
+	public int Db_TEST_Insert(String name, String value);
+	
+	/**
+	 * @return HashMap of contents from DB
+	 */
+	public HashMap<Integer, String> Db_TEST_Select();
+	
+	
+}
